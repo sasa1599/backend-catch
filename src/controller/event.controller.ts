bda72def6108c812cf8f5667e079cf55562db385
sasa1599 <Shnazzhr@gmail.com>
2024-12-19 15:41:17 +0700

    coba lagiiiiii (#9)
    * cobain Deploy
    
    * deploy lagi
    
    * coba lagi
    
    * lagi deploy

--- src/controller/event.controller.ts
+++ src/controller/event.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../prisma";
-import { Category } from "prisma/generated/client";
+import { Category } from "../../prisma/generated/client";
 
 
 export class EventController {
