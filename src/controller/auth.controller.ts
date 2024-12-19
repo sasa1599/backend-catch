1b5a7ed7d099b91247e47a7a050de978ae2e9aa2
sasa1599 <Shnazzhr@gmail.com>
2024-12-19 15:27:25 +0700

    cobain deploy lagi (#7)
    * cobain Deploy
    
    * deploy lagi

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import prisma from "../../prisma";
+import prisma from "../prisma";
 import bcrypt from "bcrypt";
 import { sign, verify } from "jsonwebtoken";
 import {
